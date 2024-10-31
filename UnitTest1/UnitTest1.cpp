263e98ecc8bdfe7eef019133edb081e266a44b4f
Imchidex <144730149+Imchidex@users.noreply.github.com>
2024-10-31 19:02:16 -0400

    Re-did the unit tests as well.

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,45 +1,52 @@
+// unittest.cpp
 #include "pch.h"
 #include "CppUnitTest.h"
-
 extern "C" {
-#include "../CSCN71020_Assignment3/main.h"
+#include "../CSCN71020_Assignment3/main.h"  // Include the main header for function access
 }
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
 {
-	TEST_CLASS(UnitTest1)
-	{
-	public:
-		
-		TEST_METHOD(Test_RockBeatsScissors)
-		{
-			char* result = determineWinner("Rock", "Scissors");
-			Assert::AreEqual("Player1", result); // Expected Player1 wins
-		}
-        TEST_METHOD(Test_ScissorsBeatsPaper)
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(Test_RockBeatsScissors) //Test for Rock vs Scissors
         {
-            char* result = determineWinner("Scissors", "Paper");
-            Assert::AreEqual("Player1", result); // Expected Player1 wins
+            char player1[] = "Rock";
+            char player2[] = "Scissors";
+            Assert::AreEqual("Player1", determineWinner(player1, player2));
         }
 
-        TEST_METHOD(Test_PaperBeatsRock)
+        TEST_METHOD(Test_ScissorsBeatsPaper)//Test for Scissors Vs Paper
         {
-            char* result = determineWinner("Paper", "Rock");
-            Assert::AreEqual("Player1", result); // Expected Player1 wins
+            char player1[] = "Scissors";
+            char player2[] = "Paper";
+            Assert::AreEqual("Player1", determineWinner(player1, player2));
         }
 
-        TEST_METHOD(Test_Draw)
+        TEST_METHOD(Test_PaperBeatsRock)//Test for Paper vs Rock
         {
-            char* result = determineWinner("Rock", "Rock");
-            Assert::AreEqual("Draw", result); // Expected Draw
+            char player1[] = "Paper";
+            char player2[] = "Rock";
+            Assert::AreEqual("Player1", determineWinner(player1, player2));
         }
 
-        TEST_METHOD(Test_InvalidInput)
+        TEST_METHOD(Test_Draw)//TESt for a draw
         {
-            char* result = determineWinner("Lizard", "Rock");
-            Assert::AreEqual("Invalid", result); // Expected Invalid
+            char player1[] = "Rock";
+            char player2[] = "Rock";
+            Assert::AreEqual("Draw", determineWinner(player1, player2));
         }
-	};
+
+        TEST_METHOD(Test_InvalidInput)//Tsey for an Invalid input
+        {
+            char player1[] = "Lizard";
+            char player2[] = "Rock";
+            Assert::AreEqual("Invalid", determineWinner(player1, player2));
+        }
+    };
 }
